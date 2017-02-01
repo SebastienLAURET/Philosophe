ef8283f0a3dd820b29b74fd88dbb47d188db50e3
MisterAlex95 <alex.dana@epitech.eu>
2017-02-01 13:58:15 -0800

    * new: add execve

---
+++ srcFork/Execve.hpp
@@ -0,0 +1,24 @@
+
+#ifndef EXECVE_HPP
+# define EXECVE_HPP
+# include <unistd.h>
+
+class Execve {
+private:
+  char *_envp[];
+  char *_argv[];
+
+public:
+  Execve(char *env[], char *argv[]) {
+    _env = env;
+  }
+
+  ~Execve(){
+  }
+
+  void run(const std::string filename){
+    execve(filename.c_str(), _argv, _envp);
+  }
+};
+
+#endif //!EXECVE_HPP
