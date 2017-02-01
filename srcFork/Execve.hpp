01e69013084e8749c5adb3008cdcb12f711fe21f
MisterAlex95 <alex.dana@epitech.eu>
2017-02-01 15:05:29 -0800

    * fix: fork && execve

--- srcFork/Execve.hpp
+++ srcFork/Execve.hpp
@@ -4,7 +4,6 @@
 
 #include <string>
 #include <vector>
-#include <list>
 #include <unistd.h>
 
 class Execve {
@@ -15,7 +14,7 @@ public:
   Execve(char **env);
   ~Execve();
 
-  void run(const std::string &filename, std::list<std::string> *argVector = NULL);
+  void run(const std::string &filename, std::vector<std::string> *argVector = NULL);
 };
 
 #endif //!EXECVE_HPP
