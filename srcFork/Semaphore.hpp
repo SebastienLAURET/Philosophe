744b8a0d99c3ee24ef878ebd66fa2bf9737d62d1
MisterAlex95 <alex.dana@epitech.eu>
2017-02-01 13:44:06 -0800

    * fix: remove run

--- srcFork/Semaphore.hpp
+++ srcFork/Semaphore.hpp
@@ -12,16 +12,13 @@ public:
   Semaphore() {
     _key = ftok(".", ID_PROJECT);
     _semId = semget(_key, 1, IPC_CREATE | IPC_EXCL | 0666);
+    semctl(_semId, 0, SET_VAL, 1);
   }
   ~Semaphore() {
     semctl(_semId, 0, IPC_RMID, 0);
   }
 
-  void run() {
-    semctl(_semId, 0, SET_VAL, 1);
-  }
-
-    void op(int seim_num, int sem_op, int sem_flg) {
+  void op(int seim_num, int sem_op, int sem_flg) {
       _op.sem_num = sem_num;
       _op.sem_op = sem_op;
       _op.sem_flg = sem_flg;
