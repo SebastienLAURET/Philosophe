5dc3c61bdfc33b0aed8862b2fa5c543850293dcb
MisterAlex95 <alex.dana@epitech.eu>
2017-02-13 12:10:53 -0800

    * new: add script

--- timer.sh
+++ timer.sh
@@ -1,6 +1,6 @@
-time ./philoThread 1> /dev/null &
+time -o ./res/thread ./philoThread 1> /dev/null &
 sleep 5
 killall philoThread
-time ./launcher 1> /dev/null &
+time -o ./res/fork  ./launcher 1> /dev/null &
 sleep 5
 killall launcher
